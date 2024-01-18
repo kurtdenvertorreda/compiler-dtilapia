792116b86e41e519d88b812e4c1330efe144f866
SpaceMonkey002 <138639636+kurtdenvertorreda@users.noreply.github.com>
2024-01-18 08:47:29 +0800

    Added responsiveness for scaling the window

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ADMIN\Desktop\try compiler\venv
+set VIRTUAL_ENV=C:\Users\katod\OneDrive - Itech\NEW Backup\Kurt Files\Kurt Files All\Github\compiler-dtilapia\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
