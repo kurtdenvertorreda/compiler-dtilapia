fe396da8f54962941cd7e00625d25effbbb178b6
Nichole <nicholenalpapara@iskolarngbayan.pup.edu.ph>
2024-01-21 07:04:48 +0800

    ok

--- sample_lexer/ppl/constants.py
+++ sample_lexer/ppl/constants.py
@@ -3,6 +3,10 @@ UPPER_CASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz'
 ALPHABET = UPPER_CASE + LOWER_CASE
 
+#######################################
+# TOKENS
+#######################################
+
 TT_INT		= 'Integer'
 TT_FLOAT    = 'Float'
 TT_STRING   = 'String'
@@ -11,25 +15,62 @@ TT_COMPL    = 'Complex'
 TT_BOOL     = 'Boolean'
 TT_SET      = 'Set'
 TT_ARR      = 'Array'
+
 TT_PLUS     = 'Addition Operator'
 TT_MINUS    = 'Subtraction Operator'
 TT_MUL      = 'Multiplication Operator'
 TT_DIV      = 'Division Operator'
+
+TT_LCBRAC   = 'Left Curly Bracket'
+TT_RCBRAC   = 'Right Curly Bracket'
+TT_LSQBRAC  = 'Left Square Bracket'
+TT_RSQBRAC  = 'Right Curly Bracket'
+
+# Arithmetic Operators
+TT_MODULO = 'Modulo Operator'
+TT_EXPONENT = 'Exponent Operator'
+
+# Comparison Operators
+TT_GREATER_THAN = 'Greater Than Operator'
+TT_LESS_THAN = 'Less Than Operator'
+TT_GREATER_THAN_EQUAL = 'Greater Than or Equal To Operator'
+TT_LESS_THAN_EQUAL = 'Less Than or Equal To Operator'
+TT_EQUAL_TO = 'Equal To Operator'
+TT_NOT_EQUAL_TO = 'Not Equal To Operator'
+
+# Logical Operators
+TT_NEGATION = 'Negation Operator'
+TT_DISJUNCTION = 'Disjunction Operator'  # For \/ or ||
+TT_CONJUNCTION = 'Conjunction Operator'  # For /\ or &&
+TT_CONDITIONAL = 'Conditional Operator'  # For ->
+TT_IMPLICATION = 'Implication Operator'  # For ==>
+TT_BICONDITIONAL = 'Bi-conditional Operator'  # For <->
+
+# Assignment Operators
+TT_ASSIGNMENT = 'Assignment Operator'  # For =
+TT_ADDITION_ASSIGNMENT = 'Addition Assignment Operator'  # For +=
+TT_SUBTRACTION_ASSIGNMENT = 'Subtraction Assignment Operator'  # For -=
+TT_MULTIPLICATION_ASSIGNMENT = 'Multiplication Assignment Operator'  # For *=
+TT_DIVISION_ASSIGNMENT = 'Division Assignment Operator'  # For /=
+TT_MODULUS_ASSIGNMENT = 'Modulus Assignment Operator'  # For %=
+
+# Unary Operators
+TT_UNARY_PLUS = 'Unary Plus Operator'  # For +
+TT_UNARY_MINUS = 'Unary Minus Operator'  # For -
+TT_INCREMENT = 'Increment Operator'  # For ++
+TT_DECREMENT = 'Decrement Operator'  # For --
+TT_FACTORIAL = 'Factorial Operator'  # For !
+
 TT_LPAREN   = 'Left Parenthesis'
 TT_RPAREN   = 'Right Parenthesis'
 TT_IDENTIFIER = 'Identifier'
 TT_KEYWORD = 'Keyword'
-TT_RESERVE = 'Reserved Words'
-TT_NOISE = 'Noise Words'
-TT_LCBRAC = 'Left Curly Bracket'
-TT_RCBRAC = 'Right Curly Bracket'
-TT_LSQBRAC = 'Left Square Bracket'
-TT_RSQBRAC = 'Right Square Bracket'
-TT_ASSOP = 'Assignment Operator'
+TT_RESERVE = 'Reserved Word'
+TT_NOISE   = 'Noise Word'
 
 KEYWORDS = {'int', 'float', 'String', 'char', 'bool', 'set', 'array', 'complex', 'let', 'be',
             'for', 'from', 'to', 'in', 'by', 'do', 'when', 'otherwise', 'funct', 'while', 'given',
-            'output', 'print', 'show', 'input', 'find', 'hence', 'integer'}
+            'output', 'print', 'show', 'input', 'find', 'hence'}
 
 RESERVED_WORDS = {'true', 'false', 'permutation', 'combination', 'btree', 'preorder', 'inorder', 'postorder',
                  'null', 'search', 'add', 'remove', 'ugraph', 'dgraph', 'nodeAdd', 'removeEdge', 'UedgeAdd', 'DedgeAdd',
