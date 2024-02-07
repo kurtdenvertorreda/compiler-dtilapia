ff4b372bbb103bb25db2085cea07c391978bb049
John Patrick Lagatuz <patrick.zutagal@gmail.com>
2024-02-08 06:57:15 +0800

    modifiedd declaration

--- dtilParser.py
+++ dtilParser.py
@@ -119,13 +119,15 @@ class Parser:
                                     return ResParse(self.current_token.line, store, f'Invalid token at line {self.current_token.line}', "Expected ']'.")
                             else:
                                 return ResParse(self.current_token.line, store, f'Invalid token at line {self.current_token.line}', "Expected ']'.")
-                        elif self.current_token.value == TT_ASSIGNMENT:
+                        elif self.current_token.type == TT_ASSIGNMENT:
                             store += str(self.current_token.value) + " "
                             self.advance()
                             if self.current_token.type == TT_INT or self.current_token.type == TT_IDENTIFIER or self.current_token.type == TT_FLOAT or self.current_token.type == TT_STRING or self.current_token.type == TT_CHAR or self.current_token.type == TT_COMPL:
                                 store += str(self.current_token.value) + " "
                                 self.advance()
                                 return ResParse(self.current_token.line, store, "No Error", "No Error")
+                            else:
+                                return ResParse(self.current_token.line, store, f'Invalid token at line {self.current_token.line}', "Expected ']'.")
                         elif self.current_token.value not in KEYWORDS_DATA_TYPE:
                             return ResParse(self.current_token.line, store, f'Invalid token at line {self.current_token.line}', "Expected ']'.")
                         else:
